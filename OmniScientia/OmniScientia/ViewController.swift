b2dcd8ab07c6b112e06a4e0e2f606d498f54179f
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-05-15 08:42:45 -0300

    Commit inicial.

---
+++ OmniScientia/OmniScientia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OmniScientia
+//
+//  Created by Mariana Alvarez on 15/05/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
