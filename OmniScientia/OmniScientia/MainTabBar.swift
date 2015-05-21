755eef2c6d2852e79ba55c2d8148c9630e3d951a
Victor Travassos <Victor.Travassos@iCloud.com>
2015-05-21 10:48:00 -0300

    Divido o Main.storyboard em outros três arquivos

---
+++ OmniScientia/OmniScientia/MainTabBar.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBar.swift
+//  OmniScientia
+//
+//  Created by Victor Travassos Ferreira on 5/21/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        var conteudoStoryboard = UIStoryboard(name: "conteudo", bundle: nil)
+        var rootConteudoVC = conteudoStoryboard.instantiateInitialViewController() as! UINavigationController
+        
+        var usuarioStoryboard = UIStoryboard(name: "usuario", bundle: nil)
+        var rootUsuarioVC = usuarioStoryboard.instantiateInitialViewController() as! UINavigationController
+        
+        var inicioStoryboard = UIStoryboard(name: "inicio", bundle: nil)
+        var rootInicioVC = inicioStoryboard.instantiateInitialViewController() as! UINavigationController
+        
+        self.setViewControllers([rootConteudoVC,rootInicioVC,rootUsuarioVC], animated: true)
+    }
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        return viewController != tabBarController.selectedViewController
+    }
+}
