962921717e8bf52eab6d90f8d8660d8eba4f8e12
Victor Travassos <victor@Victors-MacBook-Pro.local>
2015-06-22 05:05:23 -0300

    Alterações

---
+++ OmniScientia/OmniScientia/LOCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LearningObjectsCollectionViewCell.swift
+//  OmniScientia
+//
+//  Created by Victor Travassos on 6/22/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class LOCollectionViewCell: UICollectionViewCell {
+    
+}
